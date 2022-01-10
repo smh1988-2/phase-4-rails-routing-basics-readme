b3f5f9b9c577709b2644346af93ce88751b8e320
smh1988-2 <seamhurley1988@gmail.com>
2022-01-10 10:45:47 -0800

    commit tree dfa2ed587bb67071b4efc7973c5b9ceacb9e569e

---
+++ app/controllers/cheeses_controller.rb
@@ -0,0 +1,13 @@
+class CheesesController < ApplicationController
+
+    def index
+        cheeses = Cheese.all
+        render json: cheeses
+    end
+
+    def first
+        cheese = Cheese.first
+        render json: cheese
+    end
+
+end
